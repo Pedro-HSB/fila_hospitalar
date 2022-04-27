feeca05dde2772c602285ea1eb484cb5063d885b
pedro <pedro.bernardo8@etec.sp.gov.br>
2022-04-27 20:21:49 -0300

    class_create

---
+++ atvi_fila-hospitalar/lista_pacientes.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace atvi_fila_hospitalar
+{
+    class lista_pacientes
+    {
+    }
+}
